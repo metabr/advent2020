f0b94d6029762d62e673bb5aeed2a0395f469740
Aleh Suprunovich <br@ahlamon.org>
2020-12-14 10:40:59 +0300

    Day 14 part 2

--- day14.py
+++ day14.py
@@ -17,12 +17,47 @@ def input(filename="day14.txt"):
         return input.readlines()
 
 
+def int_to_list(n):
+    s = "{0:036b}".format(n)
+    return list(s)
+
+
+def list_to_int(n):
+    return int("".join(n), 2)
+
+
 def apply_mask(val, mask):
-    val = list("{0:036b}".format(val))
+    val = int_to_list(val)
     for i in range(len(val)):
         if mask[i] != "X":
             val[i] = mask[i]
-    return int("".join(val), 2)
+    return list_to_int(val)
+
+
+def flip(addr, offset):
+    addr = int_to_list(addr)
+    if addr[offset] == "0":
+        addr[offset] = "1"
+    else:
+        addr[offset] = "0"
+    return list_to_int(addr)
+
+
+def decode(addr, mask):
+    addr = int_to_list(addr)
+    for i in range(len(addr)):
+        if mask[i] == "1":
+            addr[i] = "1"
+    result = [addr]
+    for i in range(len(addr)):
+        if mask[i] == "X":
+            for j in range(len(result)):
+                x = result[j]
+                x = list_to_int(x)
+                r = flip(x, i)
+                r = int_to_list(r)
+                result.append(r)
+    return [list_to_int(x) for x in result]
 
 
 if __name__ == "__main__":
@@ -35,6 +70,8 @@ if __name__ == "__main__":
             mask = parse_mask(instr)
         else:
             addr, val = parse_mem(instr)
-            mem[addr] = apply_mask(val, mask)
+            decoded_addrs = decode(addr, mask)
+            for a in decoded_addrs:
+                mem[a] = val
 
     print(f"Sum of values in memory: {sum(mem.values())}")
