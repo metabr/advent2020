cf5652df592d0a9cff598cc7bdcab535f0a3deb2
Aleh Suprunovich <br@ahlamon.org>
2020-12-17 12:13:48 +0300

    Day 17 part 2

--- day17.py
+++ day17.py
@@ -1,13 +1,14 @@
 from collections import Counter
 
 
-def neighbor_cubes(x, y, z):
+def neighbor_cubes(x, y, z, w):
     result = []
     for x_ in [x - 1, x, x + 1]:
         for y_ in [y - 1, y, y + 1]:
             for z_ in [z - 1, z, z + 1]:
-                result.append((x_, y_, z_))
-    result.remove((x, y, z))
+                for w_ in [w - 1, w, w + 1]:
+                    result.append((x_, y_, z_, w_))
+    result.remove((x, y, z, w))
     return result
 
 
@@ -15,25 +16,25 @@ class PocketDimension:
     def __init__(self):
         self.cubes = {}
 
-    def activate(self, x, y, z):
-        self.cubes[(x, y, z)] = "#"
+    def activate(self, x, y, z, w):
+        self.cubes[(x, y, z, w)] = "#"
 
-    def deactivate(self, x, y, z):
-        self.cubes[(x, y, z)] = "."
+    def deactivate(self, x, y, z, w):
+        self.cubes[(x, y, z, w)] = "."
 
-    def is_active(self, x, y, z):
-        if self.cubes.get((x, y, z)) == "#":
+    def is_active(self, x, y, z, w):
+        if self.cubes.get((x, y, z, w)) == "#":
             return True
         return False
 
-    def get_neighbor_cubes(self, x, y, z):
+    def get_neighbor_cubes(self, x, y, z, w):
         cubes = []
-        for cx, cy, cz in neighbor_cubes(x, y, z):
-            cubes.append(self.cubes.get((cx, cy, cz), "."))
+        for cx, cy, cz, cw in neighbor_cubes(x, y, z, w):
+            cubes.append(self.cubes.get((cx, cy, cz, cw), "."))
         return cubes
 
-    def active_neighbors(self, x, y, z):
-        cubes = self.get_neighbor_cubes(x, y, z)
+    def active_neighbors(self, x, y, z, w):
+        cubes = self.get_neighbor_cubes(x, y, z, w)
         count = Counter(cubes)
         return count["#"]
 
@@ -45,15 +46,18 @@ class PocketDimension:
         xs = [0]
         ys = [0]
         zs = [0]
-        for x, y, z in self.cubes.keys():
+        ws = [0]
+        for x, y, z, w in self.cubes.keys():
             xs.append(x)
             ys.append(y)
             zs.append(z)
+            ws.append(w)
 
         return (
             (min(xs) - 1, max(xs) + 1),
             (min(ys) - 1, max(ys) + 1),
             (min(zs) - 1, max(zs) + 1),
+            (min(ws) - 1, max(ws) + 1),
         )
 
 
@@ -64,7 +68,7 @@ def read_input(filename="day17.txt"):
         for y in range(len(lines)):
             for x in range(len(lines[0].strip("\n"))):
                 if lines[y][x] == "#":
-                    pd.activate(x, y, 1)
+                    pd.activate(x, y, 1, 1)
     return pd
 
 
@@ -77,17 +81,19 @@ if __name__ == "__main__":
         x_min, x_max = r[0]
         y_min, y_max = r[1]
         z_min, z_max = r[2]
+        w_min, w_max = r[3]
         for x in range(x_min, x_max + 1):
             for y in range(y_min, y_max + 1):
                 for z in range(z_min, z_max + 1):
-                    if pd.is_active(x, y, z):
-                        n = pd.active_neighbors(x, y, z)
-                        if n == 2 or n == 3:
-                            pd_next.activate(x, y, z)
-                    else:
-                        n = pd.active_neighbors(x, y, z)
-                        if n == 3:
-                            pd_next.activate(x, y, z)
+                    for w in range(w_min, w_max + 1):
+                        if pd.is_active(x, y, z, w):
+                            n = pd.active_neighbors(x, y, z, w)
+                            if n == 2 or n == 3:
+                                pd_next.activate(x, y, z, w)
+                        else:
+                            n = pd.active_neighbors(x, y, z, w)
+                            if n == 3:
+                                pd_next.activate(x, y, z, w)
         pd = pd_next
 
     print(pd.total_active())
